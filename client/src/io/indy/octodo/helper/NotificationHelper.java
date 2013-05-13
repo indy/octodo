1443ff5252b0ea195bb2282374c6062ead0ac177
Inderjit Gill <email@indy.io>
2013-05-13 17:59:09 +0100

    custom notification styling

--- client/src/io/indy/octodo/helper/NotificationHelper.java
+++ client/src/io/indy/octodo/helper/NotificationHelper.java
@@ -2,14 +2,20 @@
 package io.indy.octodo.helper;
 
 import android.app.Activity;
+import android.view.ViewGroup.LayoutParams;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
-
+import de.keyboardsurfer.android.widget.crouton.Style.Builder;
 
 // Show the user notifications
 public class NotificationHelper {
 
     private Activity mActivity;
+
+    private static final int OCTODO_GREEN = 0xdd00eb45;
+
+    private static final Style CONFIRM = new Builder().setDuration(1000)
+            .setBackgroundColorValue(OCTODO_GREEN).setHeight(LayoutParams.WRAP_CONTENT).build();
 
     public NotificationHelper(Activity activity) {
         mActivity = activity;
@@ -20,6 +26,6 @@ public class NotificationHelper {
     }
 
     public void showConfirmation(String message) {
-        Crouton.makeText(mActivity, message, Style.CONFIRM).show();
+        Crouton.makeText(mActivity, message, NotificationHelper.CONFIRM).show();
     }
 }
