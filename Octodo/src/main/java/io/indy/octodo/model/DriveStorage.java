82a4dc4380da1b60b72c5bd3486f2d22ce4c3fdc
Inderjit Gill <email@indy.io>
2013-06-19 16:28:42 +0100

    added driveStorage stubs

--- Octodo/src/main/java/io/indy/octodo/model/DriveStorage.java
+++ Octodo/src/main/java/io/indy/octodo/model/DriveStorage.java
@@ -1,142 +1,108 @@
+/*
+ * Copyright 2013 Inderjit Gill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.indy.octodo.model;
 
 import android.util.Log;
 
-import com.google.api.client.http.ByteArrayContent;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-import com.google.api.services.drive.model.ParentReference;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DriveStorage {
 
-    private final static String TAG = "DriveStorage";
+    /*
+    re-implement the public methods from Database
+    store an instance of this class in MainController
+    (maybe store MainController in Application?)
+     */
 
-    private final static String JSON_MIMETYPE = "application/json";
-    private final static String APPDATA_FOLDER = "appdata";
+    private final String TAG = getClass().getSimpleName();
 
-    public static List<File> listAppDataFiles(Drive service) throws IOException {
+    private static final boolean D = true;
 
-        List<File> result = new ArrayList<File>();
-        Drive.Files.List request = service.files().list();
-        request.setQ("'appdata' in parents");
 
-        do {
-            try {
-                FileList files = request.execute();
-                result.addAll(files.getItems());
-                request.setPageToken(files.getNextPageToken());
-            } catch (IOException e) {
-                Log.d("MainActivity", "An error occurred: " + e);
-                request.setPageToken(null);
-                throw e;
-            }
-        } while (request.getPageToken() != null &&
-                request.getPageToken().length() > 0);
-
-        return result;
+    // Called when you no longer need access to the database.
+    public void closeDatabase() {
     }
 
-    // in practice the appdata folder will always contain json files
-    public static File createAppDataJsonFile(Drive service, String title, String json) throws IOException {
-
-        ByteArrayContent content = ByteArrayContent.fromString(JSON_MIMETYPE, json);
-
-        List<ParentReference> parents = new ArrayList<ParentReference>();
-        parents.add(new ParentReference().setId(APPDATA_FOLDER));
-
-        File config = new File();
-        config.setTitle(title);
-        config.setParents(parents);
-
-        File file;
-        try {
-            file = service.files().insert(config, content).execute();
-        } catch (IOException e) {
-            Log.d(TAG, "createAppDataJsonFile exception: " + e);
-            throw e;
-        }
-        return file;
-    }
-
-    public static File updateAppDataJsonFile(Drive service, File metadata, String json) throws IOException {
-        ByteArrayContent content = ByteArrayContent.fromString(JSON_MIMETYPE, json);
-
-        File file;
-        try {
-            file = service.files().update(metadata.getId(), metadata, content).execute();
-        } catch (IOException e) {
-            Log.d(TAG, "updateAppDataJsonFile exception: " + e);
-            throw e;
-        }
-        return file;
-    }
-
-    public static File getFileMetadata(Drive service, String fileId) throws IOException {
-        File f;
-        try {
-            f = service.files().get(fileId).execute();
-        } catch (IOException e) {
-            Log.d(TAG, "getFileMetadata exception: " + e);
-            throw e;
-        }
-        return f;
-    }
-
-    public static InputStream downloadFile(Drive service, File file) throws IOException {
-        if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
-            try {
-                HttpResponse resp =
-                        service.getRequestFactory().buildGetRequest(new GenericUrl(file.getDownloadUrl()))
-                                .execute();
-                return resp.getContent();
-            } catch (IOException e) {
-                // An error occurred.
-                Log.d(TAG, "downloadFile exception: " + e);
-                throw e;
-            }
-        } else {
-            // The file doesn't have any content stored on Drive.
-            return null;
+    public void addList(String name) {
+        if (name.equals("")) {
+            Log.d(TAG, "attempting to add a tasklist with an empty name");
+            return;
         }
     }
 
-    public static String downloadFileAsString(Drive service, File file) throws IOException {
+    public void deleteList(int id) {
+        Log.d(TAG, "deleteList: " + id);
+    }
 
-        String res = null;
+    public void addTask(Task task) {
+    }
 
-        try {
-            InputStream is = DriveStorage.downloadFile(service, file);
-
-            if (is != null) {
-
-                InputStreamReader isr = new InputStreamReader(is);
-                StringBuilder sb = new StringBuilder();
-                BufferedReader br = new BufferedReader(isr);
-                String read = br.readLine();
-
-                while (read != null) {
-                    sb.append(read);
-                    read = br.readLine();
-
-                }
-
-                res = sb.toString();
-            }
-        } catch (IOException e) {
-            Log.d(TAG, "downloadFileAsString exception: " + e);
-            throw e;
+    public void updateTaskContent(int taskId, String content) {
+        if (D) {
+            Log.d(TAG, "updateTaskContent id: " + taskId + " content: " + content);
         }
+    }
 
+    public void updateTaskState(int taskId, int state) {
+        if (D) {
+            Log.d(TAG, "updateTaskState id:" + taskId + " state: " + state);
+        }
+    }
+
+    // re-assign a task to a different tasklist
+    public void updateTaskParentList(int taskId, int taskListId) {
+        if (D) {
+            Log.d(TAG, "updateTaskParentList taskId: " + taskId + " taskListId: " + taskListId);
+        }
+    }
+
+
+    public void updateTask(Task task) {
+    }
+
+    // DELETE the specified task
+    public void deleteTask(int taskId) {
+        if (D) {
+            Log.d(TAG, "deleteTask: taskId=" + taskId);
+        }
+    }
+
+    // mark all struck tasks in the tasklist as closed
+    public void removeStruckTasks(int taskListId) {
+        if (D) {
+            Log.d(TAG, "removeStruckTasks: taskListId=" + taskListId);
+        }
+    }
+
+    // return all the tasks associated with the list
+    public List<Task> getTasks(int taskListId) {
+        List<Task> res = new ArrayList<Task>();
         return res;
     }
+
+    public List<TaskList> getTaskLists() {
+        List<TaskList> res = new ArrayList<TaskList>();
+        return res;
+    }
+
+    public List<TaskList> getDeleteableTaskLists() {
+        List<TaskList> res = new ArrayList<TaskList>();
+        return res;
+    }
+
 }
